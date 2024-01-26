9b7b54b9f25fd3ecf8acc63da53ca5d5cc5f0374
DavidReinon <davidreinongarcia@gmail.com>
2024-01-26 18:50:55 +0100

    funcio insertar nova ressenya

--- src/main/java/pelis/controller/PelisController.java
+++ src/main/java/pelis/controller/PelisController.java
@@ -23,7 +23,6 @@ public class PelisController {
 		return "Hola món!";
 	}
 
-
 	@PostMapping("/novaPeli")
 	ResponseEntity<Void> novaPeli(@RequestBody String jsonNovaPeli) {
 		if (insertarPeli(jsonNovaPeli)) {
@@ -31,6 +30,67 @@ public class PelisController {
 		} else {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
+
+	}
+
+	@PostMapping("/novaRessenya")
+	ResponseEntity<Void> novaRessenya(@RequestBody String jsonNovaPeli) {
+		if (insertarResenya(jsonNovaPeli)) {
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+	private boolean insertarResenya(String jsonNovaPeli) {
+		try {
+			JSONObject obj = new JSONObject(jsonNovaPeli);
+
+			// Validación del JSON
+			if (!obj.has("usuari") || !obj.has("id") || !obj.has("ressenya")) {
+				// Manejar caso en que los campos necesarios no estén presentes
+				return false;
+
+			}
+
+			String usuari = obj.getString("usuari");
+			String id = obj.getString("id");
+			String ressenya = obj.getString("ressenya");
+
+			File directori = new File("pelis");
+			if (!directori.exists()) {
+				return false;
+			}
+
+			// Obtener la lista de archivos en el directorio
+			String[] llistaFitxers = directori.list(new FiltreExtensio(".txt"));
+
+			// Verificar si el archivo ya existe en la lista
+			boolean archivoExistente = false;
+			for (String nombreArchivo : llistaFitxers) {
+				if (nombreArchivo.equals(id + ".txt")) {
+					archivoExistente = true;
+					break;
+				}
+			}
+
+			if (!archivoExistente) {
+				return false;
+			}
+
+			File estaPelicula = new File(directori, id + ".txt");
+
+			// Utilizar try-with-resources para garantizar el cierre adecuado del recurso
+			try (FileWriter fw = new FileWriter(estaPelicula, true)) {
+				fw.write(usuari + ": " + ressenya + "\n");
+			}
+
+		} catch (Exception e) {
+			System.err.println(e);
+			return false;
+		}
+		return true;
 
 	}
 
@@ -43,7 +103,7 @@ public class PelisController {
 			if (!obj.has("usuari") || !obj.has("titol")) {
 				// Manejar caso en que los campos necesarios no estén presentes
 				return resposta = false;
-				
+
 			}
 
 			String usuari = obj.getString("usuari");
